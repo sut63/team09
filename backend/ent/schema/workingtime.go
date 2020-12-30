8cde88ea79d17ff503a48d29845502de9fe77e59
phetpichet <phetpichet.r@gmail.com>
2020-12-30 22:42:55 +0700

    เพิ่ม Entity Workingtime และเพิ่มใน Schema ของระบบบันทึกประวัติการทำงานของแพทย์ -close #39

---
+++ backend/ent/schema/workingtime.go
@@ -0,0 +1,27 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/field"
+	"github.com/facebookincubator/ent/schema/edge"
+)
+
+// Workingtime holds the schema definition for the Workingtime entity.
+type Workingtime struct {
+	ent.Schema
+}
+
+// Fields of the Workingtime.
+func (Workingtime) Fields() []ent.Field {
+	return []ent.Field {
+		field.Time("time"),
+	}
+}
+
+// Edges of the Workingtime.
+func (Workingtime) Edges() []ent.Edge {
+	return []ent.Edge {
+		edge.To("doctors", Doctor.Type).
+		StorageKey(edge.Column("workingtime_id")),
+	}
+}
