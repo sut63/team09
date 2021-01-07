3f8a0e63ea19424c491f61734c5120950a954a9e
SaranSmr <earnfang@gmail.com>
2021-01-07 13:37:05 +0700

    เพิ่ม entity specialist -close #94

---
+++ backend/ent/schema/specialist.go
@@ -0,0 +1,27 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/field"
+	"github.com/facebookincubator/ent/schema/edge"
+)
+
+// Specialist holds the schema definition for the Specialist entity.
+type Specialist struct {
+	ent.Schema
+}
+
+// Fields of the Specialist.
+func (Specialist) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("specialist").NotEmpty(),
+	}
+}
+
+// Edges of the Specialist.
+func (Specialist) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("Special_Doctors",Special_Doctor.Type).
+		StorageKey(edge.Column("specialist_id")),
+	}
+}
