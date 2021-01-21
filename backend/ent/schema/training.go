8acfd32b799ed8e98a83ba0091a893b751210a14
Wipha26 <58970442+Wipha26@users.noreply.github.com>
2021-01-21 20:13:53 +0700

    เพิ่ท field ใน schema training.go แลป 9 -close #242

--- backend/ent/schema/training.go
+++ backend/ent/schema/training.go
@@ -1,7 +1,9 @@
 package schema
 
 import (
-	
+	"errors"
+	"regexp"
+
 	"github.com/facebookincubator/ent"
 	"github.com/facebookincubator/ent/schema/edge"
 	"github.com/facebookincubator/ent/schema/field"
@@ -15,21 +17,33 @@ type Training struct {
 // Fields of the Training.
 func (Training) Fields() []ent.Field {
 	return []ent.Field{
-		field.String("branch").NotEmpty(),
+		field.String("branch").Validate(func(s string) error {
+			match, _ := regexp.MatchString("^[ก-๏]+$", s)
+			if !match {
+				return errors.New("กรอกสาขาให้เป็นภาษาไทยเท่านั้น")
+			}
+			return nil
+		}),
 		field.Time("dateone"),
 		field.Time("datetwo"),
-		 
-    }
+		field.String("doctoridcard").MaxLen(10).MinLen(10),
+		field.String("hour").Validate(func(s string) error {
+			match, _ := regexp.MatchString("^[0-9]+$", s)
+			if !match {
+				return errors.New("กรอกชั่วโมงเป็นตัวเลขเท่านั้น")
+			}
+			return nil
+		}),
+	}
 
 }
 
 // Edges of the Training.
 func (Training) Edges() []ent.Edge {
 	return []ent.Edge{
-        edge.From("course",Course.Type).Ref("trainings").Unique(),
+		edge.From("course", Course.Type).Ref("trainings").Unique(),
 		edge.From("doctor", Doctor.Type).Ref("trainings").Unique(),
 		edge.From("department", Department.Type).Ref("trainings").Unique(),
-		      
-    }
+	}
 
 }
