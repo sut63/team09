36ea417c3ccf0885e4ea0c62f3cfd450ddc74e20
phetpichet <phetpichet.r@gmail.com>
2021-01-22 15:55:03 +0700

    Update office.go -close #253

--- backend/ent/schema/office.go
+++ backend/ent/schema/office.go
@@ -1,6 +1,9 @@
 package schema
 
 import (
+	"errors"
+	"regexp"
+
 	"github.com/facebookincubator/ent"
 	"github.com/facebookincubator/ent/schema/edge"
 	"github.com/facebookincubator/ent/schema/field"
@@ -14,7 +17,21 @@ type Office struct {
 // Fields of the Office.
 func (Office) Fields() []ent.Field {
 	return []ent.Field{
-		field.String("officename").NotEmpty(),
+		field.String("officename").Validate(func(s string) error {
+		match, _ := regexp.MatchString("^[ก-๏]+$",s)
+			if !match {
+				return errors.New("กรุณากรอกสถานที่ทำงาน")
+			}
+			return nil
+		}),
+		field.String("roomnumber").Validate(func(s string) error {
+			match, _ := regexp.MatchString("[ABC]\\d{4}",s)
+				if !match {
+					return errors.New("กรุณากรอกหมายเลขห้องทำงาน")
+				}
+				return nil
+			}),
+		field.String("doctoridcard").MaxLen(10).MinLen(10),
 		field.Time("added_time1"),
 		field.Time("added_time2"),
 	}
