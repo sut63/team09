0074d0f6a08f63676efd8a00208f72efd63f51eb
Chattrakarn <chattrakarn911@gmail.com>
2021-03-19 14:51:22 +0700

    update folder Detail และ DetailTable - close #430

--- frontend/plugins/welcome/src/components/DetailTable/Tables.tsx
+++ frontend/plugins/welcome/src/components/DetailTable/Tables.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { createStyles, fade, makeStyles, Theme } from '@material-ui/core/styles';
-import { Link as RouterLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -8,15 +7,12 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import { DefaultApi } from '../../api/apis';
-import { EntDetail } from '../../api/models/EntDetail';
-import { Content, ContentHeader, Header, Page } from '@backstage/core';
-import { pageTheme } from '@backstage/core';
-import { Link } from '@material-ui/core';
 import moment from 'moment';
-import SearchIcon from '@material-ui/icons/Search';
-import TextField from '@material-ui/core/TextField';
+import { Page, pageTheme, Header, Content } from '@backstage/core';
+import { Grid, Button, TextField, Typography, FormControl } from '@material-ui/core';
+import SearchTwoToneIcon from '@material-ui/icons/SearchTwoTone';
+import { EntDetail } from '../../api';
+import { Alert } from '@material-ui/lab';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -25,160 +21,210 @@ const useStyles = makeStyles((theme: Theme) =>
       flexWrap: 'wrap',
       justifyContent: 'center',
     },
+    headsearch: {
+      width: 'auto',
+      margin: '10px',
+      color: '#FFFFFF',
+      background: '#2196F3',
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    margins: {
+      margin: theme.spacing(2),
+    },
+
+    withoutLabel: {
+      marginTop: theme.spacing(3),
+    },
+    textField: {
+      width: '25ch',
+    },
+    paper: {
+      marginTop: theme.spacing(3),
+      marginBottom: theme.spacing(3),
+    },
     table: {
-      minWidth: 650,
-    },
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.black, 0.15),
-      '&:hover': {
-        backgroundColor: fade(theme.palette.common.black, 0.25),
-      },
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-      },
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    inputRoot: {
-      color: 'inherit',
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '30ch',
-        '&:focus': {
-          width: '20ch',
-        },
-      },
+      minWidth: 500,
     },
   }),
 );
+//------------------------------------------------------------------------------
 
-export default function ComponentsTableUser() {
+export default function ComponentsTable() {
   const classes = useStyles();
-  const http = new DefaultApi();
-  const [details, setDetail] = React.useState<EntDetail[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [search, setSearch] = React.useState('');
+  const [departmentid, setDepartmentid] = useState(String);
+  const [details, setDetail] = useState<EntDetail[]>([])
+  const [alert, setAlert] = useState(true);
+  const [status, setStatus] = useState(false);
 
-  useEffect(() => {
-    const getDetail = async () => {
-      const res = await http.listDetail({ limit: 10, offset: 0 });
-      setLoading(false);
-      setDetail(res);
-    };
-    getDetail();
-  }, [loading]);
-
-  const deleteDetails = async (id: number) => {
-    const res = await http.deleteDetail({ id: id });
-    setLoading(true);
+  //------------------------------------------------------------------------------
+  const Departmentidhandlehange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setStatus(false);
+    setDepartmentid(event.target.value as string);
   };
-
-  const filterDetail = details.filter(details => {
-    return details.edges?.department?.name?.includes(search)
-  })
-
-
-  function emptyDetail () : any { 
-    if(filterDetail.length == 0){
-      const detailed = <TableRow> <TableCell align="center" colSpan={9}><p>ไม่มีข้อมูลในระบบ</p></TableCell></TableRow>;
-      return detailed;
-    }
+  const cleardata = () => {
+    setDepartmentid("");
+    setStatus(false);
+    setDetail([]);
   }
-
+  //------------------------------------------------------------------------------
+  const SearchDetail = async () => {
+    setStatus(true);
+    setAlert(true);
+    const apiUrl = `http://localhost:8080/api/v1/detailtables?detail=${departmentid}`;
+    const requestOptions = {
+      method: 'GET',
+    };
+    fetch(apiUrl, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.data)
+        setStatus(true);
+        setAlert(false);
+        setDetail([]);
+        if (data.data != null) {
+          if (data.data.length >= 1) {
+            setStatus(true);
+            setAlert(true);
+            console.log(data.data)
+            setDetail(data.data);
+          }
+        }
+      });
+  }
+  //------------------------------------------------------------------------------
   return (
-    // <div className={classes.root}>
     <Page theme={pageTheme.website}>
       <Header
-        title={`Doctor Information`}
-        subtitle="ข้อมูลแผนกแพทย์">
-        <Button color="secondary" variant="contained" href="/home">BACK</Button>
+        title={`Department Information`}
+        subtitle="ค้นหาข้อมูลแผนกแพทย์">
+        <table>
+          <Button color="secondary" variant="contained" href="/home">BACK</Button>
+        </table>
       </Header>
       <Content>
-        <ContentHeader title="ข้อมูลแผนกแพทย์">
-          <div className={classes.search} style={{ marginRight: 10 }}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <TextField
-              name="search"
-              className={classes.inputInput}
-              style={{ marginRight: 100 }}
-              placeholder="กรุณากรอกชื่อแผนก"
-              value={search}
-              onChange={(event) => { setSearch(event.target.value); }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
-          <Link component={RouterLink} to="/detail">
-            <Button variant="contained" color="primary">
-              บันทึกข้อมูลแผนกแพทย์
-           </Button>
-          </Link>
-        </ContentHeader>
-        <TableContainer component={Paper}>
-          <Table className={classes.table} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-              <TableCell align="center">Department</TableCell>
-                <TableCell align="center">Mission</TableCell>
-                <TableCell align="center">Course</TableCell>
-                <TableCell align="center">Phone</TableCell>
-                <TableCell align="center">Email</TableCell>
-                <TableCell align="center">Detail</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-
-              {/* ต้องใช้เงื่อนไข check ว่า filterOffice.length == 0 */}
-              {emptyDetail()}
-              {filterDetail.map(item => (
-                <TableRow key={item.id}>
-                  <TableCell align="center">{item.edges?.department?.name}</TableCell>
-                  <TableCell align="center">{item.edges?.mission?.mission}</TableCell>
-                  <TableCell align="center">{item.edges?.course?.namecourse}</TableCell>
-                  <TableCell align="center">{item.phone}</TableCell>
-                  <TableCell align="center">{item.email}</TableCell>
-                  <TableCell align="center">{item.explain}</TableCell>
-                  <TableCell align="center">
-                    <Button
-                      onClick={() => {
-                        if (item.id === undefined) {
-                          return;
-                        }
-                        deleteDetails(item.id);
-                      }}
-                      style={{ marginLeft: 10 }}
-                      variant="contained"
-                      color="secondary"
-                    >
-                      Delete
-                    </Button>
-                  </TableCell>
+        <Grid container item xs={12} justify="center">
+          <Grid item xs={5}>
+            <Paper>
+              <Typography align="center" >
+                <div style={{ background: "#3792cb", height: 50 }}>
+                  <h1 style={
+                    {
+                      color: "#FFFFFF",
+                      borderRadius: 5,
+                      height: 18,
+                      padding: '0 30px',
+                      fontSize: '30px',
+                    }}>
+                    ค้นหาข้อมูลแผนกแพทย์
+            </h1>
+                </div>
+                <div>
+                  <FormControl
+                    className={classes.margin}
+                    variant="outlined"
+                  >
+                    <div className={classes.paper}>กรุณากรอกรหัสประจำแผนก</div>
+                    <TextField
+                      id="Departmentid"
+                      value={departmentid}
+                      onChange={Departmentidhandlehange || ''}
+                      type="string"
+                      size="medium"
+                      style={{ width: 300 }}
+                    />
+                  </FormControl>
+                </div>
+                <div></div>
+                <Button
+                  onClick={() => {
+                    SearchDetail();
+                  }}
+                  endIcon={<SearchTwoToneIcon />}
+                  className={classes.margins}
+                  variant="contained"
+                  style={{ background: "#3792cb", height: 40 }}>
+                  <h3
+                    style={
+                      {
+                        color: "#FFFFFF",
+                        padding: '0 10px',
+                      }
+                    }>
+                    ค้นหาข้อมูล
+            </h3>
+                </Button>
+                <Button
+                  onClick={() => {
+                    cleardata();
+                  }}
+                  className={classes.margins}
+                  variant="contained"
+                  style={{ background: "#3792cb", height: 40 }}>
+                  <h3
+                    style={
+                      {
+                        color: "#FFFFFF",
+                        padding: '0 25px',
+                      }
+                    }>
+                    เคลียร์ข้อมูล
+            </h3>
+                </Button>
+              </Typography>
+            </Paper>
+            {status ? (
+              <div>
+                {alert ? (
+                  <Alert severity="success">
+                    แสดงข้อมูลแผนกแพทย์ {departmentid}
+                  </Alert>
+                )
+                  : (
+                    <Alert severity="warning" style={{ marginTop: 20 }}>
+                      ไม่พบข้อมูลข้อมูลแผนกแพทย์
+                    </Alert>
+                  )}
+              </div>
+            ) : null}
+          </Grid>
+        </Grid>
+        <Grid className={classes.paper}>
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="left">No.</TableCell>
+                  <TableCell align="left">รหัสประจำแผนก</TableCell>
+                  <TableCell align="left">ชื่อแผนก</TableCell>
+                  <TableCell align="left">ชื่อแพทย์หัวหน้าแผนก</TableCell>
+                  <TableCell align="left">หลักสูตรอบรม</TableCell>
+                  <TableCell align="left">เบอร์โทร</TableCell>
+                  <TableCell align="left">อีเมล</TableCell>
+                  <TableCell align="left">รายละเอียดแผนก</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {details.map((item: any) => (
+                  <TableRow key={item.id}>
+                    <TableCell align="left">{item.id}</TableCell>
+                    <TableCell align="left">{item.departmentid}</TableCell>
+                    <TableCell align="left">{item.edges?.Department?.Name}</TableCell>
+                    <TableCell align="left">{item.edges?.Doctor?.name}</TableCell>
+                    <TableCell align="left">{item.edges?.Course?.namecourse}</TableCell>
+                    <TableCell align="left">{item.phone}</TableCell>
+                    <TableCell align="left">{item.email}</TableCell>
+                    <TableCell align="left">{item.explain}</TableCell>
+                    <TableCell align="left">
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
       </Content>
     </Page>
-
   );
 }
